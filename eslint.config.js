19b7bb6e500a2f23a112d509fffdcc2daf05fac5
Tserpilouski <terpilovskikirill@gmail.com>
2025-12-02 10:53:25 +0100

    feat: make change in prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import react from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import prettierConfig from 'eslint-config-prettier';
 import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
@@ -13,7 +13,7 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.strict,
-      eslintPluginPrettier,
+      prettierConfig,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
