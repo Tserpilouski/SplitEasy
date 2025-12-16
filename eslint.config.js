a2cd38b332bfe29debb6e86e102c1819b0378859
Tserpilouski <terpilovskikirill@gmail.com>
2025-12-16 22:56:48 +0100

    feat: add rules to eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -8,38 +8,38 @@ import prettierConfig from 'eslint-config-prettier';
 import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strict,
-      prettierConfig,
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'react-compiler': reactCompiler,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'react-compiler/react-compiler': 'error',
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
+    { ignores: ['dist'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.strict, prettierConfig],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'react-compiler': reactCompiler,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            'react-compiler/react-compiler': 'error',
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            'react/prop-types': 'off',
+            'no-restricted-imports': [
+                'error',
+                {
+                    patterns: [{ regex: '^@mui/[^/]+$' }],
+                },
+            ],
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    }
 );
